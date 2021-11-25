fee54822fd46b199eafd759308d63c8b13b63bfe
Vortetty <vortetty@gmail.com>
2021-11-24 21:18:13 -0800

    Add more plans to implement

--- main.py
+++ main.py
@@ -9,8 +9,29 @@ import argparse
 import traceback
 import os
 import random
+from pyaudio import PyAudio # sudo apt-get install python{,3}-pyaudio
+import math
 
-from sympy import false
+def sine_tone(frequency, duration, volume=1, sample_rate=22050):
+    n_samples = int(sample_rate * duration)
+    restframes = n_samples % sample_rate
+
+    p = PyAudio()
+    stream = p.open(format=p.get_format_from_width(1), # 8bit
+                    channels=1, # mono
+                    rate=sample_rate,
+                    output=True)
+    s = lambda t: volume * math.sin(2 * math.pi * frequency * t / sample_rate)
+    samples = (int(s(t) * 0x7f + 0x80) for t in range(n_samples))
+    for buf in zip(*[samples]*sample_rate): # write several samples at a time
+        stream.write(bytes(bytearray(buf)))
+
+    # fill remainder of frameset with silence
+    stream.write(b'\x80' * restframes)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
 try:
     parser = argparse.ArgumentParser()
