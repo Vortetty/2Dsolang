bae8b422b0e1d2a59f50e9f517050526555652aa
Vortetty <vortetty@gmail.com>
2021-11-28 17:07:33 -0800

    MORE WINDOWS SHIT LKJBSVCPILUJHBESLKJBA:JBCPUVBELLAJOIAPIO PLEASE KILL ME I HATE WINDOWS (really though i just added better windows instructions and something to ensure curses is installed)

--- run-crossplatform.bat
+++ run-crossplatform.bat
@@ -1,4 +1,21 @@
 :; # This is perfectly cross-platform, only the comments will run on linux. Just has to eb a bat so windows recognizes it as a script.
 :; python3 test_args.py "$@" && ./run.sh "$@"
 :; exit
-python main.py -n %*
+
+set exectutable = ""
+
+where /q py
+IF ERRORLEVEL 1 (
+    where /q python
+    IF ERRORLEVEL 1 (
+        ECHO Please install python 3.9 or higher.
+        EXIT /B
+    ) ELSE (
+       set executable = python
+    )
+) ELSE (
+    set executable = py
+)
+
+%executable% test_for_curses.py
+%executable% main.py -n %*
